84e39144a151d96fe533054d3c4772b737472bd3
= <aristotlepoultney@gmail.com>
2023-08-20 16:33:51 -0400

    aoc day 4 solution 1

--- src/day-4.ts
+++ src/day-4.ts
@@ -5,8 +5,8 @@ function getTotal(arr: string[]) {
     let total = 0;
     arr.forEach((line) => {
         const x = line.split(",");
-        const left = x[0].split("-");
-        const right = x[1].split("-");
+        const left = x[0].split("-").map(num => +num);
+        const right = x[1].split("-").map(num => +num);
 
         if (left[0] <= right[0] && left[1] >= right[1] || right[0] <= left[0] && right[1] >= left[1]) {
             total++;

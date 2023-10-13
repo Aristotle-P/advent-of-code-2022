0821b5203794374a03ee38ed2b96185297f76b63
= <aristotlepoultney@gmail.com>
2023-10-12 21:27:49 -0400

    more progress on day 5 solution 1

--- src/day-5.ts
+++ src/day-5.ts
@@ -1,27 +1,59 @@
 import fs from "fs";
 const lines = fs.readFileSync("inputs/day-5-tmp.txt", "utf-8").split("\n");
 lines.pop();
-const stacks = [];
-function splitInput(input: String[]) {
-    let x = false;
-    let i = 0;
-    while (x === false) {
-        if (input[i].includes("1")) {
-            i++;
-            i++;
-            for (let v = 0; v < i; v++) {
-                input.shift();
-            }
-            break;
+const stacks = [[], [], []];
+const instructions = [];
+
+// Needs to be refactored
+function createStacks(input: string[]) {
+    const regex = /[A-Z]/
+    input.forEach(x => {
+        if (!regex.test(x)) {
+            return;
         }
-        console.log(input[i]);
-        stacks.push(input[i]);
-        i++;
-    }
+        
+        if (regex.test(x[1])) {
+                stacks[0].push(x[1]);
+        }
+
+        if (regex.test(x[5])) {
+                stacks[1].push(x[5])
+        }
+
+        if (regex.test(x[9])) {
+                stacks[2].push(x[9])
+        }
+    });
 }
-splitInput(lines);
+
+function filterInstructions(input: string[]) {
+    const regex = /[1-9]/
+    const unfiltered = [];
+    input.forEach(line => {
+        if (regex.test(line)) {
+            unfiltered.push(line);
+        }
+    })
+    unfiltered.shift();
+    unfiltered.forEach(line => {
+        const filteredLine = line.replace(/[^\d]/g, '')
+        instructions.push(filteredLine);
+    })
+}
+
+createStacks(lines);
+filterInstructions(lines);
+
+function sortStacks(stack, commands) {
+    commands.forEach(command => {
+        for (let i = 1; i < command[0]; i++) {
+        }
+    })
+}
+
+
 console.log(stacks);
-console.log(lines);
+console.log(instructions);
 // Test input visualized
 //     [D]    
 // [N] [C]    
